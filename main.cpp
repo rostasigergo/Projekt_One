5293597945854a14dd0c606e328fa0968bfca933
rostasigergo <rostasigergo@gmail.com>
2015-10-26 21:09:36 +0100

    Colors done

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::cout << green << "Hello" << red << " WORLD!" << yellow;
-    //with full of colors!
+    std::cout << green << "Hello " << red << "WORLD! " << yellow << "with " << green << "full " << white << "of " << red << "colors!:)";
+
+    //Csak hogy legyen itt még 1 sor
     return a.exec();
 }
 
